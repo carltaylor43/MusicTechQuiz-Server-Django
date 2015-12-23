edb1f7688f9f096902c63865740497966c049669
Carl Taylor <carltaylor43uk@gmail.com>
2015-12-23 08:54:13 +0000

    ADDED: db for production

--- quiz_proj/settings.py
+++ quiz_proj/settings.py
@@ -80,10 +80,15 @@ WSGI_APPLICATION = 'quiz_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz_project',
+        'USER': 'carl',
+        'PASSWORD': 'quiz_database_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
